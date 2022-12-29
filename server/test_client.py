e8fd36aab49fe8f4e5b55d884061f3e65767de97
Evgenii Gusev <ged1994@mail.ru>
2022-12-29 21:21:46 +0300

    added Dasha api

--- server/test_client.py
+++ server/test_client.py
@@ -3,5 +3,6 @@ import requests
 BASE = 'http://127.0.0.1:5000/'
 
 
-response = requests.get(BASE + 'test/this_string_should_be_returned', data={'arg1': 1, 'arg2': 2}) # substitute "data" with "params" to send args in a URL query
+#response = requests.get(BASE + 'test/this_string_should_be_returned', data={'arg1': 1, 'arg2': 2}) # substitute "data" with "params" to send args in a URL query
+response = requests.post(BASE + 'api', json={'name': 'Dasha'})
 print(response.json())
