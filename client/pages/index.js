06d58426d5d3eef65264401b15eb9284dae947dd
Daria Mordvinov <daria.mordvinov@gmail.com>
2023-01-31 18:34:53 -0500

    next boiler plate

---
+++ client/pages/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Application Tracker</title>
+        <meta name="description" content="Application tracker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <main className={styles.main}>
+        <div></div>
+        <div className={styles.center}>
+          <h1>Application tracker: Landing page</h1>
+        </div>
+        <div className={styles.grid}>
+          <a
+            href=""
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Sign Up <span>-&gt;</span>
+            </h2>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Log In <span>-&gt;</span>
+            </h2>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
