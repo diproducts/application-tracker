34db1b0bf328614ba19b6d50ff040a610b2b036c
Evgenii Gusev <ged1994@mail.ru>
2024-06-12 13:18:18 +0300

    transfered contacts field from Phase to Application

--- application_tracker/applications/models/application.py
+++ application_tracker/applications/models/application.py
@@ -10,6 +10,7 @@ class Application(models.Model):
     position = models.CharField(max_length=255)
     url = models.URLField(null=True, blank=True)
     job_description = models.TextField(null=True, blank=True)
+    contacts = models.TextField(null=True, blank=True)
     cv = models.FileField('CV', null=True, blank=True, validators=[pdf_or_word_file_validator])
     cover_letter = models.FileField(null=True, blank=True, validators=[pdf_or_word_file_validator])
     offered_salary = models.PositiveIntegerField(help_text='Your offered salary', null=True, blank=True)
